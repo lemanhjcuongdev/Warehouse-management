62c61be851d9a1be2fa387d2d1ddd38a4b445a3e
cuonglm <cuong.dz.1806@gmail.com>
2024-05-08 14:26:55 +0700

    vercel config

--- server/dist/routes/user.route.js
+++ server/dist/routes/user.route.js
@@ -9,7 +9,7 @@ const authentication_1 = require("../middleware/authentication");
 const userRouter = express_1.default.Router();
 userRouter.get('/', [authentication_1.checkJwt, authentication_1.checkRole], user_controller_1.default.getAllUser);
 userRouter.get('/:id', [authentication_1.checkJwt], user_controller_1.default.getUserById);
-userRouter.post('/create-user', [authentication_1.checkJwt, authentication_1.checkRole], user_controller_1.default.createUser);
+userRouter.post('/create-user', user_controller_1.default.createUser);
 userRouter.patch('/:id', [authentication_1.checkJwt, authentication_1.checkRole], user_controller_1.default.editUserById);
 userRouter.delete('/:id', [authentication_1.checkJwt, authentication_1.checkRole], user_controller_1.default.softDeleteUserById);
 exports.default = userRouter;
