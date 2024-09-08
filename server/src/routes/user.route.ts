7bf23aa2a369c9310606ba5e86dfaad778efb512
Lê Mạnh Cường <cuong.dz.1806@gmail.com>
2024-09-08 15:50:03 +0700

    update

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -8,7 +8,7 @@ const userRouter = express.Router()
 userRouter.get('/', [checkJwt, checkRole], userController.getAllUser)
 userRouter.get('/:id', [checkJwt], userController.getUserById)
 userRouter.post('/create-user', [checkJwt, checkRole], userController.createUser)
-userRouter.patch('/:id', userController.editUserById)
+userRouter.patch('/:id', [checkJwt, checkRole], userController.editUserById)
 userRouter.delete('/:id', [checkJwt, checkRole], userController.softDeleteUserById)
 
 export default userRouter
